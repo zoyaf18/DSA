5a674675960219c9e342aaa3571eb0cae88c2ab0
Zoya Fatima <64043575+zoyaf18@users.noreply.github.com>
2023-06-09 23:20:03 +0530

    Create BubbleSort(Recursion).java

---
+++ BubbleSort(Recursion).java
@@ -0,0 +1,18 @@
+class Solution
+{
+    //Function to sort the array using bubble sort algorithm.
+	public static void bubbleSort(int arr[], int n){
+	    
+	    if (n==1) return;
+        for (int j = 0; j <= n-2 ; j++){
+            if (arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                
+            }
+        }
+        bubbleSort(arr, n-1);
+    }
+
+}
